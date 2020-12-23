e5370e6eee6ba89f67cf74e26162e4752d7b42e5
Patrick <pdf@80columns.com>
2020-12-22 23:47:02 -0500

    solved part 1!

--- 19/Program.cs
+++ 19/Program.cs
@@ -6,30 +6,32 @@ using System.Threading.Tasks;
 
 namespace _19 {
     class Program {
-        static string ParseRules(Dictionary<string, object> _rules_dictionary, object rule) {
-            if (rule.GetType() == typeof(ValueTuple<string, string>)) {
-                var rule_items = (ValueTuple<string, string>)rule;
+        static string ParseRules(Dictionary<string, object> _rules_dictionary, object _rule) {
+            if (_rule.GetType() == typeof(ValueTuple<string, string>)) {
+                var rule_items = (ValueTuple<string, string>)_rule;
                 var first_part = string.Empty;
                 var second_part = string.Empty;
 
                 if (rule_items.Item1.Contains(' ')) {
-                    first_part = ParseRules(_rules_dictionary, _rules_dictionary[rule_items.Item1.Split(' ')[0]])
-                               + ParseRules(_rules_dictionary, _rules_dictionary[rule_items.Item1.Split(' ')[1]]);
+                    foreach (var rule_part in rule_items.Item1.Split(' ')) {
+                        first_part += ParseRules(_rules_dictionary, _rules_dictionary[rule_part]);
+                    }
                 } else {
                     first_part = ParseRules(_rules_dictionary, _rules_dictionary[rule_items.Item1]);
                 }
 
                 if (rule_items.Item2.Contains(' ')) {
-                    second_part = ParseRules(_rules_dictionary, _rules_dictionary[rule_items.Item2.Split(' ')[0]])
-                                + ParseRules(_rules_dictionary, _rules_dictionary[rule_items.Item2.Split(' ')[1]]);
+                    foreach (var rule_part in rule_items.Item2.Split(' ')) {
+                        second_part += ParseRules(_rules_dictionary, _rules_dictionary[rule_part]);
+                    }
                 } else {
                     second_part = ParseRules(_rules_dictionary, _rules_dictionary[rule_items.Item2]);
                 }
 
                 return "(" + first_part + "|" + second_part + ")";
-            } else if (rule.GetType() == typeof(string)) {
-                if ((rule as string).Contains(' ')) {
-                    var rule_items = (rule as string).Split(' ');
+            } else if (_rule.GetType() == typeof(string)) {
+                if ((_rule as string).Contains(' ')) {
+                    var rule_items = (_rule as string).Split(' ');
                     var output = "(";
 
                     for (var i = 0; i < rule_items.Length; i++) {
@@ -38,24 +40,21 @@ namespace _19 {
 
                     return $"{output})";
                 } else {
-                    return "(" + ParseRules(_rules_dictionary, _rules_dictionary[(rule as string)]) + ")";
+                    return "(" + ParseRules(_rules_dictionary, _rules_dictionary[(_rule as string)]) + ")";
                 }
             } else {
-                return Convert.ToString(rule);
+                // if the rule is a single character, return it as a string
+                return Convert.ToString(_rule);
             }
         }
 
-        static async Task Main(string[] args) {
-            var input = (await File.ReadAllTextAsync("input.txt", Encoding.UTF8)).Split("\n\n");
-            
-            var rules = input[0].Split('\n', StringSplitOptions.RemoveEmptyEntries);
-            var messages = input[1].Split('\n', StringSplitOptions.RemoveEmptyEntries);
-
+        static Dictionary<string, object> GetRulesDictionary(string[] _rules) {
             var rules_dictionary = new Dictionary<string, object>();
 
-            foreach (var rule in rules) {
+            foreach (var rule in _rules) {
                 var rule_parts = rule.Split(':');
                 var rule_conditions = rule_parts[1];
+
                 var rule_value = null as object;
 
                 if (rule_conditions.Contains("|")) {
@@ -72,39 +71,24 @@ namespace _19 {
                 rules_dictionary.Add(rule_parts[0], rule_value);
             }
 
-            var parsed_rule = ParseRules(rules_dictionary, rules_dictionary["0"]);
+            return rules_dictionary;
+        }
 
-            Console.WriteLine(parsed_rule);
-
+        static int GetMatchingMessageCount(string _rule, string[] _messages) {
             var matching_message_count = 0;
 
-            foreach (var message in messages) {
+            foreach (var message in _messages) {
+                var rule_index = 0;
                 var message_index = 0;
                 var message_index_stack = new Stack<int>();
                 var previous_match = false;
-                var previous_char_right_paren = false;
 
-                Console.WriteLine($"processing message {message}...");
-                Console.WriteLine();
-
-                for (var rule_index = 0; rule_index < parsed_rule.Length && message_index < message.Length; rule_index++) {
-                    Console.WriteLine($"ri => {rule_index}");
-                    Console.WriteLine($"rule[ri] => {parsed_rule[rule_index]}");
-                    Console.WriteLine($"mi => {message_index}");
-                    Console.WriteLine($"message[mi] => {message[message_index]}");
-                    Console.WriteLine($"previous_match => {previous_match}");
-                    Console.WriteLine($"mi stack => {string.Join(",", message_index_stack)}");
-                    Console.WriteLine();
-
-                    if (parsed_rule[rule_index] == '(') {
+                for (; rule_index < _rule.Length && message_index < message.Length; rule_index++) {
+                    if (_rule[rule_index] == '(') {
                         // group open
-                        if (!previous_char_right_paren) {
-                            message_index_stack.Push(message_index);
-                        }
-
-                        previous_char_right_paren = false;
-                    } else if (parsed_rule[rule_index] == '|') {
-                        // or
+                        message_index_stack.Push(message_index);
+                    } else if (_rule[rule_index] == '|') {
+                        // or character
                         if (previous_match) {
                             // don't need to check the right-hand side of the or statement, so find the closing parentheses index of the statement
                             var found_right_paren = false;
@@ -113,60 +97,83 @@ namespace _19 {
                             while (!found_right_paren) {
                                 rule_index++;
 
-                                if (parsed_rule[rule_index] == '(') {
+                                if (_rule[rule_index] == '(') {
                                     existing_left_paren_count++;
-                                } else if (parsed_rule[rule_index] == ')') {
+                                } else if (_rule[rule_index] == ')') {
                                     if (existing_left_paren_count > 0) {
                                         existing_left_paren_count--;
                                     } else {
                                         found_right_paren = true;
+                                        message_index_stack.Pop();
                                     }
                                 }
                             }
                         }
-
-                        previous_char_right_paren = false;
-                    } else if (parsed_rule[rule_index] == ')') {
+                    } else if (_rule[rule_index] == ')') {
                         // group close
-                        previous_char_right_paren = true;
+                        message_index_stack.Pop();
                     } else {
                         // letter
-                        if (parsed_rule[rule_index] == message[message_index]) {
+                        if (_rule[rule_index] == message[message_index]) {
                             message_index++;
                             previous_match = true;
                         } else {
                             previous_match = false;
 
+                            var reset_message_index = (_rule[rule_index - 1] == '(') ? false : true;
                             var found_pipe = false;
                             var existing_left_paren_count = 0;
 
                             while (!found_pipe) {
                                 rule_index++;
 
-                                if (parsed_rule[rule_index] == '(') {
+                                if (rule_index == _rule.Length) {
+                                    break;
+                                } else if (_rule[rule_index] == '(') {
                                     existing_left_paren_count++;
-                                } else if (parsed_rule[rule_index] == ')' && existing_left_paren_count > 0) {
+                                } else if (_rule[rule_index] == ')' && existing_left_paren_count > 0) {
                                     existing_left_paren_count--;
-                                } else if (parsed_rule[rule_index] == ')' && existing_left_paren_count == 0) {
-                                    message_index = message_index_stack.Pop();
-                                } else if (parsed_rule[rule_index] == '|' && existing_left_paren_count == 0) {
+                                } else if (_rule[rule_index] == ')' && existing_left_paren_count == 0) {
+                                    message_index_stack.Pop();
+
+                                    if (message_index_stack.Count > 0) {
+                                        message_index = message_index_stack.Peek();
+                                    }
+                                } else if (_rule[rule_index] == '|' && existing_left_paren_count == 0) {
                                     found_pipe = true;
+
+                                    if (reset_message_index) {
+                                        message_index = message_index_stack.Peek();
+                                    }
                                 }
                             }
                         }
-                    
-                        previous_char_right_paren = false;
                     }
                 }
 
                 if (message_index == message.Length) {
-                    Console.WriteLine($"message {message} matched!");
-                    Console.WriteLine();
                     matching_message_count++;
                 }
             }
 
-            Console.WriteLine(matching_message_count);
+            return matching_message_count;
+        }
+
+        static async Task Main(string[] args) {
+            var input1 = (await File.ReadAllTextAsync("input1.txt", Encoding.UTF8)).Split("\n\n");
+            var rules1 = input1[0].Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            var messages1 = input1[1].Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            var rules_dictionary = GetRulesDictionary(rules1);
+            var rule = ParseRules(rules_dictionary, rules_dictionary["0"]);
+
+            Console.WriteLine(GetMatchingMessageCount(rule, messages1));
+
+            var input2 = (await File.ReadAllTextAsync("input2.txt", Encoding.UTF8)).Split("\n\n");
+            var rules2 = input1[0].Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            var messages2 = input1[1].Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            
 
             Console.WriteLine("Hello World!");
         }
